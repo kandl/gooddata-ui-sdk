828f224001ea5b06c7a9f5e7935b42883e59383d
scavnickyj <jakub.scavnicky@gooddata.com>
2023-12-07 14:23:12 +0100

    fix: overwrite existing filters

--- libs/sdk-ui-dashboard/src/model/commandHandlers/drill/crossFilteringHandler.ts
+++ libs/sdk-ui-dashboard/src/model/commandHandlers/drill/crossFilteringHandler.ts
@@ -7,16 +7,13 @@ import {
     areObjRefsEqual,
     filterAttributeElements,
     filterObjRef,
-    getAttributeElementsItems,
     isNegativeAttributeFilter,
-    updateAttributeElementsItems,
 } from "@gooddata/sdk-model";
 import compact from "lodash/compact.js";
-import uniq from "lodash/uniq.js";
 import { DashboardContext } from "../../types/commonTypes.js";
 import { selectFilterContextAttributeFilters } from "../../store/filterContext/filterContextSelectors.js";
 import { convertIntersectionToAttributeFilters } from "./common/intersectionUtils.js";
-import { addAttributeFilter, removeAttributeFilter } from "../../commands/filters.js";
+import { addAttributeFilter, changeAttributeFilterSelection } from "../../commands/filters.js";
 import { replaceInsightWidgetFilterSettings } from "../../commands/insight.js";
 import { CrossFiltering } from "../../commands/drill.js";
 
@@ -60,32 +57,16 @@ export function* crossFilteringHandler(ctx: DashboardContext, cmd: CrossFilterin
 function* applyCrossFilter(currentFilters: IDashboardAttributeFilter[], newFilter: IAttributeFilter) {
     const newFilterRef = filterObjRef(newFilter);
     const newFilterElements = filterAttributeElements(newFilter);
-    const eixstingFilterIndex = currentFilters.findIndex((item) =>
+    const existingFilter = currentFilters.find((item) =>
         areObjRefsEqual(item.attributeFilter.displayForm, newFilterRef),
     );
 
-    if (eixstingFilterIndex > -1) {
-        // TODO: handle merging positive and negative filters?
-        const existingFilter = currentFilters[eixstingFilterIndex];
-        const existingAttributeElementsItems = getAttributeElementsItems(
-            existingFilter.attributeFilter.attributeElements,
-        );
-        const newAttributeElementsItems = getAttributeElementsItems(newFilterElements);
-        const mergedAttributeElementsItems = uniq([
-            ...existingAttributeElementsItems,
-            ...newAttributeElementsItems,
-        ]);
-
-        yield put(removeAttributeFilter(existingFilter.attributeFilter.localIdentifier!));
+    if (existingFilter) {
         yield put(
-            addAttributeFilter(
-                newFilterRef,
-                eixstingFilterIndex,
-                "",
-                "multi",
-                "readonly",
-                updateAttributeElementsItems(newFilterElements, mergedAttributeElementsItems),
-                isNegativeAttributeFilter(newFilter),
+            changeAttributeFilterSelection(
+                existingFilter.attributeFilter.localIdentifier!,
+                newFilterElements,
+                isNegativeAttributeFilter(newFilter) ? "NOT_IN" : "IN",
             ),
         );
     } else {
@@ -95,7 +76,7 @@ function* applyCrossFilter(currentFilters: IDashboardAttributeFilter[], newFilte
                 currentFilters.length,
                 "",
                 "multi",
-                "readonly",
+                "active",
                 newFilterElements,
                 isNegativeAttributeFilter(newFilter),
             ),
